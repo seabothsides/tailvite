ce1ff1cba195ce0523d7d7f8de6283c17b0450cc
seabothsides <youngserge1@gmail.com>
2022-01-26 13:31:08 +0000

    init commit

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p class="text-slate-500 w-full bg-blue-200 text-4xl p-10 font-bold">
+      Nuxt-vite with tailwind/nuxt
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
